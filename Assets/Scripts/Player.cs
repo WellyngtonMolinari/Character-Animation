135033d9e8ef4cfabef00c086cb2fd4cc6fc926b
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-06-19 15:00:45 -0300

    Movement and Animations Update
    Now the Player can go any direction by pressing WASD keys and also Jump.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,83 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Animator anim;
+
+    [SerializeField] private float moveSpeed = 7f;
+    private float runningSpeed = 10f;
+    private bool isWalking;
+    private bool isRunning;
+
+    private Animator animator;
 
     private void Awake()
     {
-        anim = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
 
-    
+    private void Update()
+    {
+        Vector2 inputVector = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Jump");
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isRunning = true;
+            moveSpeed = runningSpeed;
+        }
+        else
+        {
+            isRunning = false;
+            moveSpeed = 7f; // Reset to default walking speed
+        }
+
+        // Normalize input vector
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != Vector3.zero;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+
+        // Update animator parameters
+        animator.SetBool("IsWalking", isWalking);
+        animator.SetBool("IsRunning", isRunning);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    public bool IsRunning()
+    {
+        return isRunning;
+    }
+
 }
